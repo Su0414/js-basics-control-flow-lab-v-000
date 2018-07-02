f85ca2c24854d6485d15b094e0cf7dcb939653e6
Suchitra Naik <susanaik@gmail.com>
2018-07-02 22:50:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride){
+  let result;
+
   if(ride <= 400)
-    console.log('This one is on me!')
-
-
+    result = 'This one is on me!';
+    else if (400 < ride < 2000)
+      result ='I will gladly take your thirty bucks.'
+      else if(ride >2500)
+        result ='No can do.'
+return result
 }
 
 function ternaryCheckCity(){
