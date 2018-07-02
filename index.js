f45b7c57a833f8fff3ba8cc97d1c1c8bf5aafa7e
Suchitra Naik <susanaik@gmail.com>
2018-07-02 22:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 function scuberGreetingForFeet(ride){
   if(ride <= 400)
     console.log('This one is on me!')
-    else if (400 < ride < 2000)
-      console.log('I will gladly take your thirty bucks.')
-      else if(ride >2500)
-      console.log('No can do.')
+
 
 }
 
