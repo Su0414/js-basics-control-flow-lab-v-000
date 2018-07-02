e08bd86186f40195e7bae6d8a50fa6ce7f6116a6
Suchitra Naik <susanaik@gmail.com>
2018-07-02 22:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Write your code in this file!
-function scuberGreetingForFeet(){
-  
+function scuberGreetingForFeet(ride){
+  if(ride <= 400)
+    console.log('This one is on me!')
+    else if (400 < ride < 2000)
+      console.log('I will gladly take your thirty bucks.')
+      else if(ride >2500)
+      console.log('No can do.')
+
 }
 
 function ternaryCheckCity(){
-  
+
 }
 
 function switchOnCharmFromTip(){
-  
+
 }
